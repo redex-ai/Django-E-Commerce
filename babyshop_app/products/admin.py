b493429bf930d0832355b63f5abad5b08c685a9f
redex-ai <149960054+redex-ai@users.noreply.github.com>
2023-11-11 18:26:03 +0530

    First Commit

--- babyshop_app/products/admin.py
+++ babyshop_app/products/admin.py
@@ -1,12 +1,11 @@
-from re import search
-from django.contrib import admin
-from . models import Category, Product
+from django.db import models
+from django.contrib.auth.models import User
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display=('name','price')
-    search_fields=('name',)
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields={'slug':('name',)}
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.product.name}"
