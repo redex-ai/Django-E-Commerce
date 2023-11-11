292373426d018a32d15cd26f2d2dbff1731a1c92
redex-ai <149960054+redex-ai@users.noreply.github.com>
2023-11-11 18:26:05 +0530

    First Commit

---
+++ babyshop_app/reviews/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.product.name}"
